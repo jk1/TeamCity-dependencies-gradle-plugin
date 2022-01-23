762af9c12869cd2b350fb8f32185cad4ba76c03d
Evgeny Naumenko <evgeny.naumenko@jetbrains.com>
2022-01-23 23:53:09 +0300

    #44 use repository credentials to pin builds
    (cherry picked from commit 00ca67a73de25875b6027f491c9ed912725cb372)

--- src/main/groovy/com.github.jk1.tcdeps/repository/PinConfiguration.groovy
+++ src/main/groovy/com.github.jk1.tcdeps/repository/PinConfiguration.groovy
@@ -2,8 +2,6 @@ package com.github.jk1.tcdeps.repository
 
 class PinConfiguration {
     String url
-    String username
-    String password
     boolean stopBuildOnFail
     boolean pinEnabled
     String message
